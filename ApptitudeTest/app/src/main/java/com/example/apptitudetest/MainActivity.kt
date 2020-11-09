d2f2a03d1515075f2719875e826abfd6af19678e
samdsg <boltcode17@gmail.com>
2020-11-09 09:31:22 -0800

    with kotlin quiz

---
+++ ApptitudeTest/app/src/main/java/com/example/apptitudetest/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.apptitudetest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide();
+
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button);
+
+        button.setOnClickListener {
+            val intent = Intent(this, Stats::class.java)
+            startActivity(intent)
+        }
+    }
+}
